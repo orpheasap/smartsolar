2ad4dad71689709b1da01edc9f63238631a9c935
protogelrafinas <57870984+protogelrafinas@users.noreply.github.com>
2020-01-02 16:15:56 +0200

    Add files via upload

---
+++ code/temp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define SIZE 1
+#define NUMELEM 74
+
+int main ()
+{
+	FILE *fp = NULL;
+	char buff[100];
+	char temp_raw[7];
+	float temp_c;
+	memset(buff,0,sizeof(buff));
+	memset(temp_raw,0,sizeof(temp_raw));
+	/*Enable Temp*/
+	system("sudo modprobe w1-gpio");
+	system("sudo modprobe w1-therm");
+
+for(;;)
+{
+	
+	fp = fopen("/sys/bus/w1/devices/28-01144eec7eaa/w1_slave","r");
+
+	if (NULL == fp)
+	{
+	printf("\n fopen Error!!!\n\n");
+	return 1;
+	}
+
+	if(SIZE*NUMELEM != fread(buff,SIZE,NUMELEM,fp))
+	{
+	printf("\n fread() failed\n");
+	return 1;
+	}
+
+	temp_raw[0] = buff[69];
+	temp_raw[1] = buff[70];
+	temp_raw[2] = buff[71];
+	temp_raw[3] = buff[72];
+	temp_raw[4] = buff[73];
+	temp_raw[5] = buff[74];
+	temp_c=atof(temp_raw)/1000;
+	printf("\nTemp Is: %f DegC\n", temp_c); 
+
+}
+	return 0;
+}
